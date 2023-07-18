11b4bfa9debd1420672773b87070f62b12b77301
Nyarn-WTF <dev@mail.nyarn.wtf>
2023-07-18 14:38:35 +0000

    なんか動いたｳｪｰｲ🎉

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include "esp_task_wdt.h"
 #include <M5Stack.h>
 #include <Adafruit_NeoPixel.h>
 #include <micro_ros_platformio.h>
@@ -33,29 +34,38 @@ float Kd = 0.2;
 double wheel_phi = 0.20;
 double wheel_tread = 0.20;
 
+int16_t target_vl = 0;
+int16_t target_vr = 0;
+
 int16_t pid_calc(int16_t befor_value, int16_t feedback_rpm, int16_t target_rpm, int16_t error[2]);
 void motor_control(void * pvParameters);
 void ros2_setup();
+void ros2_loop(void *pvParameters);
 
-SET_LOOP_TASK_STACK_SIZE(64000);
+SET_LOOP_TASK_STACK_SIZE(16000);
 
-#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
-#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop_rc();}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop_soft();}}
 
 #define M5STACK_FIRE_NEO_NUM_LEDS 10
 #define M5STACK_FIRE_NEO_DATA_PIN 15
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(M5STACK_FIRE_NEO_NUM_LEDS, M5STACK_FIRE_NEO_DATA_PIN, NEO_GRB + NEO_KHZ800);
 
-void error_loop(){
+void error_loop_rc(){
   while(1){
-    int r = 255;
-    int g = 0;
-    int b = 0;
-    for(int i = 0; i < 9; i++){
-        pixels.setPixelColor(i, pixels.Color(r, g, b));   
-    }  
-    pixels.show();
+    M5.Lcd.clear(BLACK);
+    M5.Lcd.setCursor(10, 10);
+    M5.Lcd.printf("RC Error");
+    delay(100);
+  }
+}
+
+void error_loop_soft(){
+  while(1){
+    M5.Lcd.clear(BLACK);
+    M5.Lcd.setCursor(10, 10);
+    M5.Lcd.printf("Soft Error");
     delay(100);
   }
 }
@@ -65,14 +75,6 @@ void setup() {
     M5.Power.begin();
     Serial.begin(2000000);
     Serial.println("setup start");
-
-    int r = 0;
-    int g = 0;
-    int b = 0;
-    for(int i = 0; i < 9; i++){
-        pixels.setPixelColor(i, pixels.Color(r, g, b));   
-    }  
-    pixels.show();
 
     delay(1000);
 
@@ -108,7 +110,13 @@ void setup() {
 
     ros2_setup();
 
-    xTaskCreatePinnedToCore(motor_control,  "motor control", 32000, nullptr, 1, nullptr, 0);
+    for(int i = 0; i < 12; i++){
+        pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+        pixels.show();  
+    }  
+
+    xTaskCreatePinnedToCore(motor_control,  "motor control", 16000, nullptr, 2, nullptr, 0);
+    xTaskCreatePinnedToCore(ros2_loop,  "ros2 loop", 16000, nullptr, 2, nullptr, 1);
 }
 
 void loop() {
@@ -116,22 +124,12 @@ void loop() {
     M5.update();
 
     if(!motor->get_timeout()){
-        double vr = -(double)motor->get_right_wheel_feedback().velocity / 60 * wheel_phi * PI;
-        double vl = (double)motor->get_left_wheel_feedback().velocity / 60 * wheel_phi * PI;
-        double x = (vr + vl) / 2;
-        double z = (vr - vl) / (2 * wheel_tread);
-        odom_msg.linear.x = x;
-        odom_msg.angular.z = z;
-        RCSOFTCHECK(rcl_publish(&publisher, &odom_msg, NULL));
         M5.Lcd.clear(BLACK);
         M5.Lcd.setCursor(10, 10);
-        M5.Lcd.printf("Twixt\nLX: %lf\nAZ: %lf\n", cmd_vel_msg.linear.x, cmd_vel_msg.angular.z);
-        M5.Lcd.printf("Odom \nLX: %lf\nAZ: %lf\n", x, z);
-    }else{
-        odom_msg.linear.x = 0;
-        odom_msg.angular.z = 0;
+        M5.Lcd.printf("Cmd \nLX: %lf\nAZ: %lf\n", cmd_vel_msg.linear.x, cmd_vel_msg.angular.z);
+        M5.Lcd.printf("Odom \nLX: %lf\nAZ: %lf\n", odom_msg.linear.x, odom_msg.angular.z);
+        M5.Lcd.printf("RPM \nRF: %d RT: %d\nLT: %d LR: %d\n", motor->get_left_wheel_feedback().velocity, target_vl, motor->get_right_wheel_feedback().velocity, target_vr);
     }
-    RCCHECK(rclc_executor_spin_one_period(&executor, 1));
     delay(1);
 }
 
@@ -150,8 +148,8 @@ void motor_control(void * pvParameters){
             if(average_count >= 10){
                 float vl = cmd_vel_msg.linear.x - cmd_vel_msg.angular.z * wheel_tread;
                 float vr = cmd_vel_msg.linear.x + cmd_vel_msg.angular.z * wheel_tread;
-                int16_t target_vl = vl * 60 / wheel_phi / PI;
-                int16_t target_vr = vr * 60 / wheel_phi / PI;
+                target_vl = vl * 60 / wheel_phi / PI;
+                target_vr = vr * 60 / wheel_phi / PI;
 
                 left_velo = pid_calc(left_velo, left_velo_ave, target_vl, left_err);
                 right_velo = pid_calc(right_velo, right_velo_ave, -target_vr, right_err);
@@ -168,6 +166,8 @@ void motor_control(void * pvParameters){
             left_velo_ave = 0;
             right_velo_ave = 0;
             average_count = 0;
+            target_vl = 0;
+            target_vr = 0;
             left_err[0] = 0;
             left_err[1] = 0;
             right_err[0] = 0;
@@ -176,7 +176,6 @@ void motor_control(void * pvParameters){
 
         motor->set_velocity(left_velo, right_velo);
         motor->drive();
-
         delay(1);
     }
 }
@@ -245,4 +244,24 @@ void ros2_setup(){
     // create executor
     RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
     RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &cmd_vel_msg, &subscription_callback, ON_NEW_DATA));
+}
+
+void ros2_loop(void *pvParameters){
+    while(1){
+        if(!motor->get_timeout()){
+            double vr = -(double)motor->get_right_wheel_feedback().velocity / 60 * wheel_phi * PI;
+            double vl = (double)motor->get_left_wheel_feedback().velocity / 60 * wheel_phi * PI;
+            double x = (vr + vl) / 2;
+            double z = (vr - vl) / (2 * wheel_tread);
+            odom_msg.linear.x = x;
+            odom_msg.angular.z = z;
+            rcl_publish(&publisher, &odom_msg, NULL);
+        }else{
+            odom_msg.linear.x = 0;
+            odom_msg.angular.z = 0;
+        }
+        rclc_executor_spin_some(&executor, RCL_MS_TO_NS(1000));
+        Serial.flush();
+        delay(100);
+    }
 }
